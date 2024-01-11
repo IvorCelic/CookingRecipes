efade85102dc3b26a902f841a1b52bf7cede5524
Ivor Ćelić <ivorcelic@gmail.com>
2024-01-11 16:44:16 +0100

    Initial commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using CookingRecipes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookingRecipes.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+
+            return View(category);
+        }
+    }
+}
